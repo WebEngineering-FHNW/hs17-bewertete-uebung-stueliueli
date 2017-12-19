125d5ead9ccebcb114c5da56093846da9dacbe64
Manuel Stutz <manuel.stutz@students.fhnw.ch>
2017-12-19 17:36:58 +0100

    Add ControllerTest

--- src/test/groovy/mvc/SocrativeLightControllerSpec.groovy
+++ src/test/groovy/mvc/SocrativeLightControllerSpec.groovy
@@ -1,6 +1,7 @@
 package mvc
 
 import grails.test.mixin.TestFor
+import grails.web.servlet.mvc.GrailsParameterMap
 import spock.lang.Specification
 
 /**
@@ -8,15 +9,34 @@ import spock.lang.Specification
  */
 @TestFor(SocrativeLightController)
 class SocrativeLightControllerSpec extends Specification {
-
+    SocrativeLightController controller
     def setup() {
+        controller = new SocrativeLightController()
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "test get intParam"(){
+        when:
+            params.put('intVal1', 5)
+            params.put('intVal2', 4356731)
+            params.put('intVal3', -234)
+            params.put('nonIntVal1', "asdf")
+            params.put('nonIntVal1', new ArrayList<String>())
+            params.put('nonIntVal1', 4.5f)
+        then:
+            controller.intParam(params, 'intVal1') == 5
+            controller.intParam(params, 'intVal2') == 4356731
+            controller.intParam(params, 'intVal3') == -234
+            shouldFail(IllegalArgumentException) {
+                controller.intParam(params, 'nonIntVal1')
+            }
+            shouldFail(IllegalArgumentException) {
+                controller.intParam(params, 'nonIntVal2')
+            }
+            shouldFail(IllegalArgumentException) {
+                controller.intParam(params, 'nonIntVal3')
+            }
     }
 }
